69f63c7e68287c36ec69d933264216dfd0624ebc
Noex98 <johannes.knickering@hotmail.com>
2021-09-23 17:29:07 +0200

    new workout html, css, js

--- frontend/src/views/Plans.js
+++ frontend/src/views/Plans.js
@@ -1,12 +1,11 @@
 import Header from "../components/Header.js"
 import Nav from "../components/Nav.js";
 import Background from "../components/Background.js"
-import { plans } from "../Store.js";
+import { user } from "../Store.js";
 import __ENV from "../env.js";
 
 export default function Plans(){
 
-    console.log(plans.data())
 
     window.updatePlans = () => {
 
@@ -30,14 +29,74 @@ export default function Plans(){
             .then(data => console.log(data))
     }
 
+    let exerciseCount = 1
+
+    window.addExercise = () => {
+        exerciseCount += 1
+        document.getElementById('exerciseCount'). innerText = `${exerciseCount} exercises`
+
+        let newRow = /*html*/`
+            <tr>
+                <td>
+                    <input type="text" placeholder="Untitled" />
+                </td>
+                <td>
+                    <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/>
+                </td>
+                <td>
+                    <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/>
+                </td>
+                <td>
+                    <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/> 
+                </td>
+            </tr>
+        `
+        let tableBody = document.querySelector('tbody')
+
+        tableBody.innerHTML += newRow
+
+    }
+
     return (/*html*/ `
     ${Background()}
     <div id="view__plans">
         ${Header({backBtn: true, profileBtn: true})}
         <h2>Create your workout</h2>
+        
         <div id="plans__inputCont">
             <input class="inputCont__name" type="text" placeholder="Untitled" />
-            <button onclick="updatePlans()">Finish</button>
+            <div id="exerciseCount">1 exercise</div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>exercise</th>
+                        <th>set</th>
+                        <th>rep</th>
+                        <th>weight</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="text" placeholder="Untitled" />
+                        </td>
+                        <td>
+                            <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/>
+                        </td>
+                        <td>
+                            <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/>
+                        </td>
+                        <td>
+                            <input class="td__numberInput" min="0" max="999" type="number" placeholder="x"/> 
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <img id="addBtn" src="../media/images/icons/add.svg" alt="icon" onclick="addExercise()" />
+
+            <button id="finishBtn" onclick="updatePlans()">Finish</button>
         </div>
     </div>
     ${Nav('plans')}
