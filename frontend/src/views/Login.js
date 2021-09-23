96a36bfd45263a86f34cd9000695e1080ce40b79
Noex98 <johannes.knickering@hotmail.com>
2021-09-23 03:45:31 +0200

    Login styling

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -28,48 +28,62 @@ export default function Login(){
                 }
             })
     }
+
+    window.showPasword = () => {
+        let input = document.getElementById("password");
+        let btn = document.getElementById('showPassword')
+        if (input.type === "password") {
+          input.type = "text";
+          btn.src = "../media/images/icons/eye_closed.svg"  
+        } else {
+          input.type = "password";
+          btn.src = "../media/images/icons/eye.svg"
+        }
+    }
     
 
     return (/*html*/ `
-        ${Background()}
         <div id="view__login">
+            ${Background()}
             <h1 class="logo" >Liftr</h1>
             <div class="login__container">
-                <h2>Login</h2>
+                <h2>Log in</h2>
                 <p>Start your training and see your progress!</p>
+
                 <div class="container__inputs">
                     <div>
-                        <img src="../media/images/icons/username2.svg" alt="icon" />
+                        <img src="../media/images/icons/username.svg" alt="icon" />
                         <div class="inputs__fields">
-                            <label for="user">Username / E-mail: </label>
-                            <input type="text" name="user" id="user" />
-                        </div>
-                    </div>
-                    <div>
-                        <img src="../media/images/icons/mail.svg" alt="icon" />
-                        <div class="inputs__fields">
-                            <label for="password">Password: </label>
-                            <input type="password" name="password" id="password" />
+                            <label for="user">Username or email: </label>
+                            <input autocomplete="off" placeholder="email@email.com" type="text" name="user" id="user" />
                         </div>
                     </div>
                     <div>
                         <img src="../media/images/icons/password.svg" alt="icon" />
                         <div class="inputs__fields">
-                            <input type="checkbox" />
-                            <label for="remember">Remember me: </label>
+                            <label for="password">Password: </label>
+                            <input placeholder="password" type="password" name="password" id="password" />
                         </div>
+                        <img onclick="showPasword()" id="showPassword" src="../media/images/icons/eye.svg" alt="icon" />
                     </div>
-                <div>
-                    <button onclick="login()">Login</button>
-                    <div id="errLog"></div>
                 </div>
+
+                <div class="container__remember">
+                    <input type="checkbox" id="remember"/>
+                    <label for="remember">Remember me </label>
+                </div>
+
+                <button  class="container__loginBtn"onclick="login()">Login</button>
                 
+                <div id="errLog"></div>
+                
+
                 ${Link('/forgot-password', /*html*/`
-                    Forgot you password?
+                    <div class="container__link">Forgot you password?</div>
                 `)}
 
                 ${Link('/new-user', /*html*/`
-                    <div>Signup here!</div>    
+                    <div class="container__link">Don't have an account? <strong>Sign up</strong></div>    
                 `)}
             </div>
         </div>
